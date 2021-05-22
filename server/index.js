1788de8675371c3dbf33ddbe41dcfccbd010d1f1
Vibhu Bhatia <vibhubhatia0007@gmail.com>
2021-05-22 12:37:19 +0530

    api setup

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-;
 
 app.use('/posts', postRoutes);
 
@@ -25,4 +24,4 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
   .catch((error) => console.log(`${error} did not connect`));
 
 
-  mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false);
