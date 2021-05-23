3aedb827f49ea43d5686a4c3302ff01f1a2bedc6
Vibhu Bhatia <vibhubhatia0007@gmail.com>
2021-05-23 19:27:40 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,12 +15,13 @@ app.use(cors());
 
 
 app.use('/posts', postRoutes);
+app.get('/',(req,res) => {
+  res.send('Welcome to fitbit backend API');
+})
 
-
-const CONNECTION_URL = 'mongodb+srv://vibhu_bit:vibhu007@cluster0.mjwpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL , { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
