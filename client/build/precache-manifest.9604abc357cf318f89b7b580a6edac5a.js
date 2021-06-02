f19bc0acf0c28f366c62440c00370c859e22bf34
Vibhu Bhatia <vibhubhatia0007@gmail.com>
2021-06-02 14:29:45 +0530

    build

---
+++ client/build/precache-manifest.9604abc357cf318f89b7b580a6edac5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c14b391b9c127722dedc30616a0a7e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71554e33a9561f9ce20",
+    "url": "/static/css/main.95e1a443.chunk.css"
+  },
+  {
+    "revision": "c7c6b45db375ebf5a31f",
+    "url": "/static/js/2.5e672b23.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.5e672b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71554e33a9561f9ce20",
+    "url": "/static/js/main.c291bc7d.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "905edc4b309aa9df000060209e8662fc",
+    "url": "/static/media/header.905edc4b.png"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "85a931279346a514527eb16dfb4d7dc7",
+    "url": "/static/media/memoriesText.85a93127.png"
+  }
+]);
